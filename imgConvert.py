2f704be2f957bdf39cea9f063fa64388029f4af9
Jiewen Guan <heiye9608@gmail.com>
2021-10-29 14:29:15 +0800

    cvt avif

--- imgConvert.py
+++ imgConvert.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from PIL import Image
-
+import pillow_avif
 
 def convert_to_webp(source):
     """Convert image to webp.
@@ -11,15 +11,15 @@ def convert_to_webp(source):
     Returns:
         pathlib.Path: path to new image
     """
-    destination = source.with_suffix(".webp")
+    destination = source.with_suffix(".avif")
 
     image = Image.open(source)  # Open image
-    image.save(destination, format="webp")  # Convert image to webp
+    image.save(destination, format="avif")  # Convert image to webp
 
     return destination
 
 def main():
-    paths = Path("assets/img/about").glob("**/*.jpg")
+    paths = Path("assets/img").glob("**/*.png")
     for path in paths:
         webp_path = convert_to_webp(path)
         print(webp_path)
